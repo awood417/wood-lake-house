7a789254f2adc52c2e25fb41979371669bd12c43
awood417 <awoo417@wgu.edu>
2026-08-17 17:08:31 -0500

    pyspark scripts

--- python_scripts/machine_learning_curated.py
+++ python_scripts/machine_learning_curated.py
@@ -27,22 +27,22 @@ DEFAULT_DATA_QUALITY_RULESET = """
 """
 
 # Script generated for node AWS Glue Data Catalog
-AWSGlueDataCatalog_node1786907336560 = glueContext.create_dynamic_frame.from_catalog(database="wood", table_name="step_trainer_trusted", transformation_ctx="AWSGlueDataCatalog_node1786907336560")
+AWSGlueDataCatalog_node1787001125113 = glueContext.create_dynamic_frame.from_catalog(database="wood", table_name="accelerometer_trusted", transformation_ctx="AWSGlueDataCatalog_node1787001125113")
 
 # Script generated for node AWS Glue Data Catalog
-AWSGlueDataCatalog_node1786907337525 = glueContext.create_dynamic_frame.from_catalog(database="wood", table_name="accelerometer_trusted", transformation_ctx="AWSGlueDataCatalog_node1786907337525")
+AWSGlueDataCatalog_node1787001125813 = glueContext.create_dynamic_frame.from_catalog(database="wood", table_name="step_trainer_trusted", transformation_ctx="AWSGlueDataCatalog_node1787001125813")
 
 # Script generated for node SQL Query
-SqlQuery1368 = '''
-select DISTINCT s.*
-FROM myDataSource s
-JOIN myDataSource2 a
-  ON s.sensorReadingTime = a.timeStamp;
+SqlQuery1855 = '''
+select * from MyDataSource a
+INNER JOIN myDataSource2 s
+  ON a.timeStamp = s.sensorReadingTime;
+
 '''
-SQLQuery_node1786907386674 = sparkSqlQuery(glueContext, query = SqlQuery1368, mapping = {"myDataSource":AWSGlueDataCatalog_node1786907336560, "myDataSource2":AWSGlueDataCatalog_node1786907337525}, transformation_ctx = "SQLQuery_node1786907386674")
+SQLQuery_node1787001257811 = sparkSqlQuery(glueContext, query = SqlQuery1855, mapping = {"myDataSource":AWSGlueDataCatalog_node1787001125113, "myDataSource2":AWSGlueDataCatalog_node1787001125813}, transformation_ctx = "SQLQuery_node1787001257811")
 
 # Script generated for node Amazon S3
-EvaluateDataQuality().process_rows(frame=SQLQuery_node1786907386674, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1786907328430", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
-AmazonS3_node1786907950218 = glueContext.write_dynamic_frame.from_options(frame=SQLQuery_node1786907386674, connection_type="s3", format="json", connection_options={"path": "s3://wood-lake-house/step_trainer/curated/", "partitionKeys": []}, transformation_ctx="AmazonS3_node1786907950218")
+EvaluateDataQuality().process_rows(frame=SQLQuery_node1787001257811, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1787001118544", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+AmazonS3_node1787001521041 = glueContext.write_dynamic_frame.from_options(frame=SQLQuery_node1787001257811, connection_type="s3", format="json", connection_options={"path": "s3://wood-lake-house/machine_learning_curated/", "partitionKeys": []}, transformation_ctx="AmazonS3_node1787001521041")
 
 job.commit()
